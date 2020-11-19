edb8b3c2fb6959974c81e0eff74aafd6b5ee1c6a
NikProgramming <NikProgramming@users.noreply.github.com>
2020-11-19 16:47:45 -0500

    change max and min

--- A5 HiLo Game/A5 HiLo Game/HiloGame.aspx.cs
+++ A5 HiLo Game/A5 HiLo Game/HiloGame.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -29,6 +30,40 @@ namespace A5_HiLo_Game
         protected void choiceCheck_Click(object sender, EventArgs e)
         {
             string guess = ChoiceBox.Text;
+            string pattern = "/$[0-9]+^/";
+            string cookieRnd = Request.Cookies.Get("rndNum").Value;
+            int maxNum = Int32.Parse(Request.Cookies.Get("maxNum").Value);
+            int minNum = Int32.Parse(Request.Cookies.Get("minNum").Value);
+
+            Regex reg = new Regex(pattern);
+            if(reg.IsMatch(guess))
+            {
+                int guessNum = Int32.Parse(guess);
+                int randomNum= Int32.Parse(cookieRnd);
+                HttpCookie max = Request.Cookies.Get("maxNum");
+                HttpCookie min = Request.Cookies.Get("minNum");
+
+                if (guessNum==randomNum)
+                {
+
+                }
+                else if((guessNum<randomNum) && (guessNum>=minNum))
+                {
+                    min.Value =(guessNum+1).ToString();
+                }
+                else if ((guessNum > randomNum) && (guessNum <= maxNum))
+                {
+                    max.Value= (guessNum - 1).ToString();
+                }
+                else
+                {
+                    //tell user its invalid
+                }
+            }
+            else
+            {
+                //tell user its invalid
+            }
         }
 
         protected void ChoiceBox_TextChanged(object sender, EventArgs e)
